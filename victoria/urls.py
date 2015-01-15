3c75700d6757badbbef9ea078b7f03466f8b47dc
reg3x <diegoaguilera85@gmail.com>
2015-01-15 09:21:06 -0430

    added routes for restapi

--- victoria/urls.py
+++ victoria/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from restapi.views import ProductList, ProductDetail
+
+product_urls = patterns('',
+    url(r'^$', ProductList.as_view(), name='product-list'),
+    url(r'^/(?P<pk>\d+)$', ProductDetail.as_view(), name='product-detail')
+)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'victoria.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^products', include(product_urls))
 )
