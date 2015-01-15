a5c94d129bae90f19e35c7ef214857282743c509
reg3x <diegoaguilera85@gmail.com>
2015-01-15 15:34:33 -0430

    added navbar and carousel directives

--- app/app.js
+++ app/app.js
@@ -1,5 +1,18 @@
 var vicApp = angular.module('vicApp', ['vicController']);
 
+vicApp.directive('navbar', function () {
+    return {
+        'restrict': 'E',
+        'templateUrl': 'components/navbar/navbar.html'
+    };
+});
+
+vicApp.directive('carousel', function () {
+    return{
+        'restrict': 'E',
+        'templateUrl': 'components/carousel/carousel.html',
+    };
+});
 
 //add config for headers
 //add SPA router
