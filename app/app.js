7c79e7ff231fa49de5772b5fdd06c32f3798730c
reg3x <diegoaguilera85@gmail.com>
2015-01-23 11:12:39 -0430

    added directives for home

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var vicApp = angular.module('vicApp', [
     'ngRoute',
     'vicController',
+    'homeDirectives',
+    'catalogDirectives'
 ]);
 
 vicApp.directive('navbar', function () {
@@ -10,52 +12,15 @@ vicApp.directive('navbar', function () {
     };
 });
 
-vicApp.directive('carousel', function () {
-    return {
-        'restrict': 'E',
-        'templateUrl': 'components/home/carousel/carousel.html'
-    };
-});
 
-vicApp.directive('marketing', function () {
-    return {
-        'restrict': 'E',
-        'templateUrl': 'components/home/marketing/marketing.html',
-        'controller': ['$scope','$http', function ($scope, $http) {
-            $http.get('components/home/marketing/marketing.json').success(function (data) {
-                console.log('$http success loading marketing.json');
-                $scope.products = data;
-            }).error( function(){
-                console.log('$http error loading marketing.json');
-                }
-            );            
-            $http.get('components/home/marketing/featurette.json').success(function (data) {
-                console.log('$http success loading featurette.json');
-                $scope.featurette = data;
-            }).error( function(){
-                console.log('$http error loading featurette.json');
-                }
-            );
-            $http.get('components/home/carousel/carousel.json').success(function (data) {
-                console.log('$http success loading carousel.json');
-                $scope.banners = data;
-            }).error( function(){
-                console.log('$http error loading carousel.json');
-                }
-            );
-        }],
-        'controllerAs': 'markets' 
-    };
-
-});
 
 vicApp.config(['$routeProvider',function ($routeProvider) {
     $routeProvider.
         when('/', {
             templateUrl: '/components/home/home.html',
         }).
-        when('/catalogo', {
-            templateUrl: 'catalogo.html'
+        when('/catalog', {
+            templateUrl: '/components/catalog/catalog.html'
         }).
         otherwise({
             redirectTo: '/'
