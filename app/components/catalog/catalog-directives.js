aab91d7ed67775fa13fe2c8873bcc1a97b8eb292
reg3x <diegoaguilera85@gmail.com>
2015-01-23 14:05:01 -0430

    added catalog mock data

---
+++ app/components/catalog/catalog-directives.js
@@ -0,0 +1,29 @@
+var vicApp = angular.module('catalogDirectives', [ ]);
+
+vicApp.directive('catcontainer', function () {
+    return{
+        restrict : 'E',
+        templateUrl: '/components/catalog/catcontainer.html',
+        controller : ['$http', '$scope','$routeParams', function ($http, $scope, $routeParams) {
+            
+            if (typeof $routeParams.product === 'undefined') {
+                console.dir('no parameter provided: '+ typeof $routeParams.product);
+                }
+            else {
+                console.dir('parameter provided: '+ $routeParams.product);
+                
+                $http.get('/components/catalog/'+$routeParams.product+'.json').
+                success(function(data) {
+                    console.log($routeParams.product +".json loaded successfuly");
+                    console.log("parameters: "+ $routeParams.product);
+
+                    $scope.product = data;
+                }).
+                error(function () {
+                    console.log("{{$routeParams.product}}.json NOT loaded successfuly");        
+                });
+            }
+
+        }],
+    }
+});
