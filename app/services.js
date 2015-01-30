d1a37007216074d2bc7920484442cef687edf118
reg3x <diegoaguilera85@gmail.com>
2015-01-30 13:10:45 -0430

    updated factories

--- app/services.js
+++ app/services.js
@@ -1,5 +1,25 @@
 vicServices = angular.module('vicServices', [])
 
-.factory('Panty', function ($resource) {
+.factory('pantyService', function ($resource) {
     return $resource('http://127.0.0.1:8000/catalog/panty');
+})
+
+.factory('brasierService', function ($resource) {
+    return $resource('http://127.0.0.1:8000/catalog/brasier');
+})
+
+.factory('leggingService', function ($resource) {
+    return $resource('http://127.0.0.1:8000/catalog/legging');
+})
+
+.factory('creamService', function ($resource) {
+    return $resource('http://127.0.0.1:8000/catalog/cream');
+})
+
+.factory('butterService', function ($resource) {
+    return $resource('http://127.0.0.1:8000/catalog/butter');
+})
+
+.factory('fraganceService', function ($resource) {
+    return $resource('http://127.0.0.1:8000/catalog/fragance');
 });
