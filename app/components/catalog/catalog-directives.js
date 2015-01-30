7658f59d04d5f6dcd5b55bb02190075b9ce42c8b
reg3x <diegoaguilera85@gmail.com>
2015-01-30 13:50:19 -0430

    added new services

--- app/components/catalog/catalog-directives.js
+++ app/components/catalog/catalog-directives.js
@@ -4,39 +4,80 @@ vicApp.directive('catcontainer', function () {
     return{
         restrict : 'E',
         templateUrl: '/components/catalog/catalog-container.html',
-        controller : ['$http', '$scope','$routeParams', 'Panty', function ($http, $scope, $routeParams, Panty) {
+        controller : ['$http', '$scope','$routeParams', 'pantyService','brasierService','leggingService',
+                      'creamService','butterService','fraganceService', 
+                      function ($http, $scope, $routeParams, pantyService,brasierService, leggingService,
+                                 creamService, butterService, fraganceService) {
             
-            if (typeof $routeParams.product === 'undefined') {
-                console.dir('no parameter provided: '+ typeof $routeParams.product);
+            if (typeof $routeParams.section === 'undefined') {
+                console.dir('no parameter provided: '+ typeof $routeParams.section);
 
                 $http.get('/components/catalog/all.json').
                 success(function(data) {
                     console.log("all.json loaded successfuly");
-                    console.log("parameters: "+ $routeParams.product);
+                    console.log("parameters: "+ $routeParams.section);
                     $scope.product = data;
                 }).
                 error(function () {
-                    console.log("{{$routeParams.product}}.json NOT loaded successfuly");        
+                    console.log("{{$routeParams.section}}.json NOT loaded successfuly");        
                 });
                 
             }
             else {
-                console.dir('parameter provided: '+ $routeParams.product);
-                      
+                console.dir('parameter provided: '+ $routeParams.section);
+                
+                
+                switch($routeParams.section) {
+                    case 'panty':
+                        pantyService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                        
+                    case 'brasier':
+                        brasierService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                        
+                    case 'legging':
+                        leggingService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                        
+                    case 'cream':
+                        creamService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                        
+                        case 'butter':
+                        butterService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                        
+                    case 'fragance':
+                        fraganceService.query(function (data) {
+                              $scope.product = data;
+                        });
+                        break;
+                }
+                
+                
+                
                 //added to test REST API
-                Panty.query(function (data) {
-                      $scope.product = data;
-                });
-
+                
                 /* --- this code works with the mockup data
-                $http.get('/components/catalog/'+$routeParams.product+'.json').
+                $http.get('/components/catalog/'+$routeParams.section+'.json').
                 success(function(data) {
-                    console.log($routeParams.product+".json loaded successfuly");
-                    console.log("parameters: "+ $routeParams.product);
+                    console.log($routeParams.section+".json loaded successfuly");
+                    console.log("parameters: "+ $routeParams.section);
                     $scope.product = data;
                 }).
                 error(function () {
-                    console.log("{{$routeParams.product}}.json NOT loaded successfuly");        
+                    console.log("{{$routeParams.section}}.json NOT loaded successfuly");        
                 });
                 */  
             }
